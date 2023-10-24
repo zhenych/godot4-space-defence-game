ddb796529f1ebc12b5a7b991459f0e94db437fe2
zhenych <droid.shmtrx@gmail.com>
2023-10-24 20:16:54 +0300

    enemy spawn and movement

--- elements/enemy/enemy.gd
+++ elements/enemy/enemy.gd
@@ -1,4 +1,13 @@
 extends CharacterBody2D
 
+@onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_right = $RayCastRight
+
 func destroy():
 	queue_free()
+
+func _physics_process(delta):
+	if ray_cast_left.is_colliding() or ray_cast_right.is_colliding():
+		get_tree().call_group("enemy_group", "change_direction")
+
+
---
+++ elements/enemy_group/enemy_group.gd
@@ -0,0 +1,36 @@
+extends Node2D
+# Enemies creation
+const POSITION_INIT_X = [ 25, 40, 25]
+const POSITION_INIT_Y = [ 15, 40, 65]
+const POSITION_STEP = 30.0
+# Movement
+const ROW_STEP = 10.0
+const SPEED_BOOST = 2.5
+ 
+@onready var enemy = preload("res://elements/enemy/enemy.tscn")
+@onready var block_timer = $BlockTimer
+
+var direction = Vector2.RIGHT
+var speed = 5.0
+
+func _ready():
+	create_enemies( 3)
+
+func _process(delta):
+	global_position += direction * speed * delta
+
+func change_direction():
+	if block_timer.time_left > 0:
+		return
+	block_timer.start()
+	direction = Vector2.LEFT if direction == Vector2.RIGHT else Vector2.RIGHT
+	global_position.y += ROW_STEP
+	speed += SPEED_BOOST
+
+func create_enemies( rows: int):
+	for row in range (rows):
+		for i in range ( 7):
+			var item: CharacterBody2D = enemy.instantiate()
+			item.position.y = POSITION_INIT_Y[row]
+			item.position.x =  POSITION_INIT_X[row] + i * POSITION_STEP
+			add_child(item)
